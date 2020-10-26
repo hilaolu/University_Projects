1ab7e77a05ee7b104ef44dac4fa3cfd13a2ddedc
hilaolu <i@phyzait.moe>
2020-10-26 11:05:47 +0800

    Update CodeTest.c

--- CodeTest.c
+++ CodeTest.c
@@ -20,7 +20,7 @@ int main()
     int a, b;
     long G;
     printf("Please input a and b:\n");
-    while((scanf("%d,%d", &a, &b) == 2) && (a >= b))
+    while(!((scanf("%d,%d", &a, &b) == 2) && (a >= b)))
     {
        clear();
     }
